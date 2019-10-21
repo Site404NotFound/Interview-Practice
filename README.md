93cb78140c01ad2795a7a06038d94efa1ede13dd
James Hippler <james.hippler@logicmonitor.com>
2019-10-21 10:10:02 -0500

    Switched code from right rotation back to original left rotation version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Interview-Practice
-An assortment of algorithms, data structures, and practice problems intended for technical interview preparation. 
+An assortment of algorithms, data structures, and practice problems intended for technical interview preparation.
+
+## Hackerrank Questions
+1. [Left Rotation](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links)
--- left_rotations/rotations_hippler.py
+++ left_rotations/rotations_hippler.py
@@ -1,5 +1,11 @@
 #! /usr/bin/env python3
 
+'''
+Hackerrank Question:
+https://www.hackerrank.com/challenges/array-left-rotation/problem
+'''
+
+
 def main():
     specs, numb_list = read_file("data.txt")
     sorted_data = rotations(int(specs[0]), int(specs[1]), numb_list)
@@ -10,12 +16,16 @@ def read_file(filename):
         data = fp.read().splitlines()
     return data[0].split(), data[1].split()
 
+# Commented sections reverse rotation and make right instead of left
 def rotations(length, operations, numb_list):
     for x in range(operations):
-        temp = numb_list[length - 1]
+        temp = numb_list[0]
+        # temp = numb_list[length - 1]
         for y in range(length - 1):
-            numb_list[length - 1 - y] = numb_list[length - 1 - y - 1]
-        numb_list[0] = temp
+            numb_list[y] = numb_list[y + 1]
+            # numb_list[length - 1 - y] = numb_list[length - 1 - y - 1]
+        numb_list[length - 1] = temp
+        # numb_list[0] = temp
     return numb_list
 
 def print_results(numb_list):
