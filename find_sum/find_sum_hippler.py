c6d1c769b50b6583963a3e296da566864cda7657
Devin <ddcast@gmail.com>
2019-10-22 16:57:23 -0500

    Updated spacing to match other python solutions

--- find_sum/find_sum_hippler.py
+++ find_sum/find_sum_hippler.py
@@ -10,18 +10,18 @@ Array = [2, 7, 5, 3, 2]
 
 
 def find_sum(sum, numb_list):
-  index = 0
-  result = 0
-  numbers = 0
-  while index < len(numb_list):
-    result += numb_list[index]
-    number += 1
-    if results <= sum:
-      if sum == 10 and index >= 3: 
-        return True
-      else:
-        index += 1
-    else: 
-      results = 0
-      numbers = 0
-  return False
+    index = 0
+    result = 0
+    numbers = 0
+    while index < len(numb_list):
+        result += numb_list[index]
+        number += 1
+        if results <= sum:
+            if sum == 10 and index >= 3: 
+                return True
+            else:
+                index += 1
+        else: 
+            results = 0
+            numbers = 0
+    return False
--- zero_sort/zero_sort_hippler.py
+++ zero_sort/zero_sort_hippler.py
@@ -13,11 +13,11 @@ Sample Output:
 
 
 def zero_sort(numb_list):
-  zero_count = 0
-  if not numb_list.contains(0): return numb_list
-  for x in range(len(numb_list)):
-    if numb_list[x] == 0: zero_count += 1
-    else:
-      numb_list[x - zero_count] = numb_list[x]
-  for y in range(len(numb_list) - zero_count, len(numb_list)):
-    numb_list[y] = 0
+    zero_count = 0
+    if not numb_list.contains(0): return numb_list
+    for x in range(len(numb_list)):
+        if numb_list[x] == 0: zero_count += 1
+        else:
+            numb_list[x - zero_count] = numb_list[x]
+    for y in range(len(numb_list) - zero_count, len(numb_list)):
+        numb_list[y] = 0
