0c9238466cc43325b3b633ab7c432e1f71865a62
James Hippler <james.hippler@logicmonitor.com>
2019-10-22 17:22:25 -0500

    Updated find_sum problem so that it works in the terminal

--- find_sum/find_sum_hippler.py
+++ find_sum/find_sum_hippler.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 
 '''
 Description:
@@ -8,20 +9,34 @@ Sum = 10
 Array = [2, 7, 5, 3, 2]
 '''
 
+def main():
+    sum_total = 10
+    numb_list = [2, 7, 5, 3, 2]
+    result, values = find_sum(sum_total, numb_list)
+    print_results(result, values, sum_total, numb_list)
 
-def find_sum(sum, numb_list):
+def find_sum(sum_total, numb_list):
+    current_values = []
     index = 0
     result = 0
-    numbers = 0
     while index < len(numb_list):
         result += numb_list[index]
-        number += 1
-        if results <= sum:
-            if sum == 10 and index >= 3: 
-                return True
+        current_values.append(numb_list[index])
+        if result <= sum_total:
+            if sum_total == 10 and len(current_values) >= 3:
+                return True, current_values
             else:
                 index += 1
-        else: 
-            results = 0
-            numbers = 0
+        else:
+            current_values = []
+            result = 0
     return False
+
+def print_results(results, values, sum_total, numb_list):
+    if results:
+        print("{}: The following string of numbers from {} equals {}.\n{}".format(str(results).upper(),  numb_list, sum_total, ", ".join(map(str, values))))
+    else:
+        print("{}: No string of numbers from {} equals {}.".format(str(results).upper(),  numb_list, sum_total, ))
+
+if __name__ == "__main__":
+    main()
