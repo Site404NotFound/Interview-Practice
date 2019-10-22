2ae9729babf9b2a181d511aeca53db52b61637a6
James Hippler <james.hippler@logicmonitor.com>
2019-10-22 17:40:09 -0500

    Updated zero_sort problem so that it works in the terminal

--- zero_sort/zero_sort_hippler.py
+++ zero_sort/zero_sort_hippler.py
@@ -1,4 +1,4 @@
-
+#! /usr/bin/env python3
 
 '''
 Description:
@@ -12,12 +12,33 @@ Sample Output:
 '''
 
 
+def main():
+    numb_list = [[0, 5, 6, -1, 0, 0, -4, 5, 1],
+                 [5, 6, -1, -4, 5, 1],
+                 [0],
+                 [1],
+                 [0, 0, 0, 0, 0, 1, 0, 0]]
+    for x in numb_list:
+        print_results("Original", x)
+        zero_sort(x)
+        print_results("Sorted", x)
+
 def zero_sort(numb_list):
     zero_count = 0
-    if not numb_list.contains(0): return numb_list
-    for x in range(len(numb_list)):
-        if numb_list[x] == 0: zero_count += 1
-        else:
-            numb_list[x - zero_count] = numb_list[x]
-    for y in range(len(numb_list) - zero_count, len(numb_list)):
-        numb_list[y] = 0
+    if 0 not in numb_list or len(numb_list) <= 1:
+        return numb_list
+    else:
+        for x in range(len(numb_list)):
+            if numb_list[x] == 0:
+                zero_count += 1
+            else:
+                numb_list[x - zero_count] = numb_list[x]
+        for y in range(len(numb_list) - zero_count, len(numb_list)):
+            numb_list[y] = 0
+    return numb_list
+
+def print_results(version, numb_list):
+    print("{} List: {}".format(version, ", ".join(map(str, numb_list))))
+
+if __name__ == "__main__":
+    main()
