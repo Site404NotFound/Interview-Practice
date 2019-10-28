2292f8d40a39d1577c57a08a9d4187c651f45fba
James Hippler <james.hippler@logicmonitor.com>
2019-10-27 19:45:40 -0500

    Finished 'Mark and Toys' Hackerrank problem in python

---
+++ mark_and_toys/mark_and_toys_hippler.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+def main():
+    data = read_file("data.txt")
+    specs = clean_data(data)
+    print(determine_max_toys(int(specs[0][0]), int(specs[0][1]), list(map(int, specs[1]))))
+
+def read_file(filename):
+    with open(filename) as fp:
+        data = fp.read().splitlines()
+    return data
+
+def clean_data(data):
+    cleansed = []
+    for line in data:
+        cleansed.append(line.split())
+    return cleansed
+
+def determine_max_toys(toys, budget, prices):
+    max_toys = 0
+    total_spent = 0
+    prices.sort()
+    for price in prices:
+        if total_spent + price <= budget:
+            total_spent += price
+            max_toys += 1
+        else:
+            return max_toys
+
+if __name__ == "__main__":
+    main()
