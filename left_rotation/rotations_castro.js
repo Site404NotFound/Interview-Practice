1a96d0e5337d9f448cd45094f2853104cdb1eef8
Devin <ddcast@gmail.com>
2019-11-06 13:27:09 -0600

    Updated castro's Left Rotation solution to work

--- left_rotation/rotations_castro.js
+++ left_rotation/rotations_castro.js
@@ -6,24 +6,20 @@ const input = fs.readFileSync('data.txt');
 let args = input.toString().split("\n");
 
 function rotations(spec, initialState) {
-  const args = spec.split(' ');
-  const rotate = args[0];
-  
-  let rotations = args[1];
-  let list = initialState.split(' ');
-
-  const rotateIndex = list.indexOf(rotate);
-  const newRotateIndex = rotateIndex - (rotate - 1);
-  const over = list.length - rotations;
-
   const state = {};
 
-  state[rotate] = newRotateIndex;
+  const args = spec.split(' ');
+  const length = args[0];
+  const rotations = args[1];
+  const list = initialState.split(' ');
+  const over = rotations - length;
 
   list.forEach((num, index) => {
-    if (num !== rotate) {
-      state[num] = index + over;
-    }
+    let newIndex = index - over;
+
+    return state[num] = (newIndex > length - 1)
+      ? 0
+      : newIndex;
   });
 
   const result = Object.keys(state).sort((a, b) => state[a] > state[b]);
