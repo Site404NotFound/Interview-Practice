8216ac6ac4fbdf57b533d1a2a26d6c4893bbdfde
ddcast <ddcast@gmail.com>
2019-11-24 14:41:58 -0600

    Made castro's solution to Merge two linked lists generate lists from input file

--- merge_two_sorted_linked_lists/merge_linked_lists_castro.js
+++ merge_two_sorted_linked_lists/merge_linked_lists_castro.js
@@ -1,5 +1,72 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
+const buff = fs.readFileSync('data.txt');
+const data = buff.toString().split("\n");
+
+let index = 1;
+let lists = null;
+
+for (const i in Array(parseInt(data[0])).fill()) {
+  lists = [new SinglyLinkedList(), new SinglyLinkedList()];
+
+  for (const listIndex in lists) {
+    index = makeLinkedList(lists[listIndex], data, index);
+  }
+
+  let current = mergeLists(lists[0], lists[1]).head;
+
+  const out = [];
+
+  while (current) {
+    out.push(getData(current));
+    current = current.next;
+  }
+
+  console.log(out.join(' '));
+}
+
+function Node(data, next) {
+  this.data = data;
+  this.next = next;
+}
+
+function SinglyLinkedList() {
+  this.head = null;
+
+  this.addNode = (data) => {
+    if (!this.head) {
+      this.head = new Node(data);
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = new Node(data);
+    }
+  };
+}
+
+function makeLinkedList(list, data, index) {
+  let size = parseInt(data[index]);
+  let start = index + 1;
+  let end = start + size;
+
+  let c = end - start;
+  let i = start;
+
+  while (c) {
+    list.addNode(parseInt(data[i]));
+    c--;
+    i++;
+  }
+
+  return index + size + 1;
+}
+
 function getData(node = {}) {
   return node.data;
 }
@@ -12,11 +79,11 @@ function shouldMerge(head1, head2) {
   return getData(head1) <= getData(head2) && getData(head2) <= getNextData(head1);
 }
 
-function mergeLists(head1, head2) {
-  let current1 = head1;
-  let current2 = head2;
+function mergeLists(list1, list2) {
+  let current1 = list1.head;
+  let current2 = list2.head;
   let tail = null;
-  
+
   while (current1) {
     if (shouldMerge(current1, current2)) {
       tail = current2.next;
@@ -24,7 +91,7 @@ function mergeLists(head1, head2) {
       current1.next = current2;
       current2 = tail;
     }
-    
+
     if (!current1.next) {
       current1.next = tail;
       break;
@@ -32,28 +99,6 @@ function mergeLists(head1, head2) {
 
     current1 = current1.next;
   }
-  
-  current1 = head1;
-  
-  while (current1) {
-    console.log(getData(current1));
-    current1 = current1.next;
-  }
+
+  return list1;
 }
-
-// TODO: Build singly linked lists from data.txt
-
-mergeLists({
-  data: 1,
-  next: {
-    data: 2,
-    next: {
-      data: 3
-    }
-  }
-}, {
-  data: 3,
-  next: {
-   data: 4
-  }
-});
